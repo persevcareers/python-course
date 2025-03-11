34bcb84bab063d7ab9f3e8e202eb573bf6bd4d41
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-03-11 13:06:10 +0530

    Update and rename js-forloops.js to 5-js-forloops.js

---
+++ JS/5-js-forloops.js
@@ -0,0 +1,9 @@
+for(i=1;i<=5;i++){
+  console.log("value of i: " +i)
+}
+=======================================
+
+greetings = "WELCOME ALL"
+for(i=0;i< greetings.length ;i++){
+  console.log(greetings[i])
+}
--- JS/js-forloops.js
+++
@@ -1,3 +0,0 @@
-for(i=1;i<=5;i++){
-  console.log("value of i: " +i)
-}
