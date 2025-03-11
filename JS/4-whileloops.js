9ed39d32d2f44a7580528758e3a73eb03dc53527
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-03-11 12:29:02 +0530

    Update 4-whileloops.js

--- JS/4-whileloops.js
+++ JS/4-whileloops.js
@@ -4,3 +4,26 @@ while( i <= 5) {
   console.log("Value of i is " +i);
   i = i+1;
 }
+============================================================
+#Without Break
+
+var i = 0;
+while( i <= 5) {
+  console.log("Value of i is " +i);
+  i = i+1;
+  if (i == 2) {
+    console.log("VALUE OF X :  " +i)
+  }
+}
+
+============================================================
+#With Break
+
+var i = 0;
+while( i <= 5) {
+  console.log("Value of i is " +i);
+  i = i+1;
+  if (i == 2) {
+    console.log("VALUE NOW  :  " +i)
+  }
+}
