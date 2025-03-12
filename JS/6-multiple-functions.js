e99115539471abb5fd5f6e47318879ae35c0b499
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-03-12 15:28:13 +0530

    Create 6-multiple-functions.js

---
+++ JS/6-multiple-functions.js
@@ -0,0 +1,15 @@
+function square(s) {
+  var sqresult = s * s ;
+  return "sqresult " +sqresult;
+}
+
+function tri(t) {
+  var triresult = t * t ;
+  return "triresult " +triresult;
+}
+  
+function quad(q) {
+  var qdresult = q * q ;
+  return "qdresult " +qdresult; 
+  return "triresult " +triresult;
+}
