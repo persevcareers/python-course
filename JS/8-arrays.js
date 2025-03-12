3ada00353a25c730b9277e3133b980f66038e16d
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-03-12 15:57:00 +0530

    Create 8-arrays.js

---
+++ JS/8-arrays.js
@@ -0,0 +1,16 @@
+var stream1 = "CSE"
+
+var stream2 = "IT"
+
+var stream3 = "MECH"
+
+console.log(stream1)
+
+console.log(stream2)
+
+console.log(stream3)
+
+
+branches = ["CSE,IT,MECH"]
+
+console.log("Different Branches available are " +branches)
