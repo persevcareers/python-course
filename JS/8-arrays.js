f0571c2959369a26f9d127097bfdceebe74cdbfd
persevcareers <148257733+persevcareers@users.noreply.github.com>
2025-03-12 19:13:33 +0530

    Update 8-arrays.js

--- JS/8-arrays.js
+++ JS/8-arrays.js
@@ -14,3 +14,22 @@ console.log(stream3)
 branches = ["CSE","IT","MECH"]
 
 console.log("Different Branches available are " +branches)
+=================================================================================
+// modifying elemnts of an array.
+var stream1 = "CSE"
+var stream2 = "IT"
+var stream3 = "MECH"
+console.log(stream1[0]);
+
+branches = ["CSE","IT","MECH"]
+console.log("Different Branches available are " +branches)
+
+branches[0] = "CS";
+console.log(branches)
+================================================================================
+var stream1 = "CSE"
+var stream2 = "IT"
+var stream3 = "MECH"
+console.log(stream1[0]);
+stream1[0]="I";
+console.log(stream1[0]);
